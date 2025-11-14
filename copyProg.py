3c0337558da5fb933b171a76ec2a6c65826c8a71
U-james-PC\james <james.merc@yahoo.com>
2025-11-14 02:30:44 -0500

    added -old flag to remove colors, changed f-strings for python 3.8 compatibility

--- copyProg.py
+++ copyProg.py
@@ -5,42 +5,39 @@ import os, sys, shutil, time
 chunk = 1024**2 * 8
 
 # On termnials that support 256 colors
-RED  = '\033[38;5;160m'
-BRED = '\033[48;5;52m'
+colors = {
+"RED":  "\033[38;5;160m",
+"BRED": "\033[48;5;52m",
 
-GRN  = '\033[38;5;34m'
-BGRN = '\033[48;2;0;30;0m'
+"GRN":  "\033[38;5;34m",
+"BGRN": "\033[48;2;0;30;0m",
 
-BLU  = '\033[38;5;26m'
-BBLU = '\033[48;2;0;0;30m'
+"BLU":  "\033[38;5;26m",
+"BBLU": "\033[48;2;0;0;30m",
 
-YEL  = '\033[38;5;220m'
-BYEL = '\033[48;5;3m'
+"YEL":  "\033[38;5;220m",
+"BYEL": "\033[48;5;3m",
 
-PURP = '\033[38;5;91m'
-BPUR = '\033[48;5;53m'
+"PURP": "\033[38;5;91m",
+"BPUR": "\033[48;5;53m",
 
-GREY = '\033[38;5;230m'
-BGRY = '\033[48;5;235m'
+"GREY": "\033[38;5;230m",
+"BGRY": "\033[48;5;235m",
 
-END  = '\033[0m'
+"END": "\033[0m",
+"CLEAR": "\033[K"
+}
 
-#_BLOCKS = ['░', '▒', '▓', '█']
-#_BLOCKS = [' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█']
-_BLOCKS = [' ', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█']
-#_BLOCKS = [' ', '╸', '━']
-#_BLOCKS = ['\033[38;5;236m━\033[0m', '\033[38;5;34m━\033[0m']
+#_BLOCKS = ["░", "▒", "▓", "█"]
+#_BLOCKS = [" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"]
+_BLOCKS = [" ", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"]
+#_BLOCKS = [" ", "╸", "━"]
+#_BLOCKS = ["\033[38;5;236m━\033[0m", "\033[38;5;34m━\033[0m"]
 
 _NUM_BLOCKS = len(_BLOCKS) - 1
 
 bar_width = os.get_terminal_size().columns // 4
 total_steps = bar_width * _NUM_BLOCKS
-
-# Convert block colors
-c = BLU
-b = BBLU
-for i in range(len(_BLOCKS)):
-    _BLOCKS[i] = f'{b}{c}{_BLOCKS[i]}{END}'
 
 
 def progress_percentage(perc, speed, eta):
@@ -54,12 +51,11 @@ def progress_percentage(perc, speed, eta):
     remainder = filled_steps % _NUM_BLOCKS
 
     if remainder > 0 and full_blocks < bar_width:
-        bar = f'{_BLOCKS[-1] * full_blocks}{_BLOCKS[remainder]}{_BLOCKS[0] * (bar_width - full_blocks - 1)}'
-
+        bar = f"{_BLOCKS[-1] * full_blocks}{_BLOCKS[remainder]}{_BLOCKS[0] * (bar_width - full_blocks - 1)}"
     else:
-        bar = f'{_BLOCKS[-1] * full_blocks}{_BLOCKS[0] * (bar_width - full_blocks)}'
+        bar = f"{_BLOCKS[-1] * full_blocks}{_BLOCKS[0] * (bar_width - full_blocks)}"
 
-    sys.stdout.write(f'\r{bar} ┋ {YEL}{perc:5.2f}%{END} ┋ {RED}{speed}MB/s{END} ┋ {PURP}{eta[0]}m {eta[1]}s{END}\033[K')
+    sys.stdout.write(f"\r{bar} ┋ {colors['YEL']}{perc:5.2f}%{colors['END']} ┋ {colors['RED']}{speed}MB/s{colors['END']} ┋ {colors['PURP']}{eta[0]}m {eta[1]}s{colors['END']}{colors['CLEAR']}")
     sys.stdout.flush()
 
 
@@ -87,19 +83,19 @@ def copyfileobj(fsrc, fdst, total, length=chunk):
             speed = bytes_since_update / time_since_update
             #h, m = divmod(m, 60)
 
-            progress_percentage(100*copied/total, f'{(speed / 1048576):6.2f}', (m, s))
+            progress_percentage(100*copied/total, f"{(speed / 1048576):6.2f}", (m, s))
 
             last_copied = copied
             last_update = elap_total
         if elap_total - last_update2 >= 1:
             eta = (total - copied) / speed
             m, s = divmod(int(eta), 60)
-            progress_percentage(100*copied/total, f'{(speed / 1048576):6.2f}', (m, s))
+            progress_percentage(100*copied/total, f"{(speed / 1048576):6.2f}", (m, s))
             last_update2 = elap_total
 
 
-    progress_percentage(total, f'{(speed / 1048576):5.2f}', (m, s))
-    print(f'\n{round(total / 1048576,3)}MB copied in {round(elap_total,3)}s')
+    progress_percentage(total, f"{(speed / 1048576):5.2f}", (m, s))
+    print(f"\n{round(total / 1048576,3)}MB copied in {round(elap_total,3)}s")
 
 
 def copyfile(src, dst, *, follow_symlinks=True):
@@ -126,29 +122,29 @@ def copyfile(src, dst, *, follow_symlinks=True):
         os.symlink(os.readlink(src), dst)
     else:
         size = os.stat(src).st_size
-        with open(src, 'rb') as fsrc:
-            with open(dst, 'wb') as fdst:
+        with open(src, "rb") as fsrc:
+            with open(dst, "wb") as fdst:
                 copyfileobj(fsrc, fdst, total=size)
     return dst
 
 
 def copy_with_progress(src, dst, *, follow_symlinks=True):
-    '''
+    """
         Wrapper for copyfile
         src: source path as string
         dst: destination path as string
-    '''
-    print(f'Copying {src} -> {dst}')
+    """
+    print(f"Copying {src} -> {dst}")
     copyfile(src, dst, follow_symlinks=follow_symlinks)
     shutil.copymode(src, dst)
 
 
 def folderCopy(r, s, dst):
-    '''
+    """
         Folder to folder copy
         r: path of source
         s: list of source files
-    '''
+    """
     x=0
     for i in s:
         if os.path.isdir(i):
@@ -170,26 +166,43 @@ def is_network_location_linux(path):
         st = os.statvfs(path)
         # Check for common network filesystem types
         # This is a simplified check and may not cover all cases
-        if st.f_fstypename in ['nfs', 'cifs', 'fuse.sshfs']:
+        if st.f_fstypename in ["nfs", "cifs", "fuse.sshfs"]:
             return True
         else:
             # You might need to parse /etc/mtab for more specific cases
-            # or use external tools like 'mount'
+            # or use external tools like "mount"
             return False
     except OSError:
         # Handle cases where statvfs might fail (e.g., inaccessible path)
         return False
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 3:
+if __name__ == "__main__":
+    if len(sys.argv) >= 3:
         src = sys.argv[1]
         dst = sys.argv[2]
 
-        if sys.platform == 'win32':
-            if r'\\' in dst or r'\\' in src:
+        if '-net' in sys.argv:
+            chunk = 1024**2 * 64
+
+        if '-old' in sys.argv:
+            _BLOCKS = ["░", "▒", "▓", "█"]
+            _NUM_BLOCKS = len(_BLOCKS) - 1
+            total_steps = bar_width * _NUM_BLOCKS
+            for c in colors:
+                colors[c] = ''
+        else:
+            # Convert block colors
+            c = colors['BLU']
+            b = colors['BBLU']
+            for i in range(len(_BLOCKS)):
+                _BLOCKS[i] = f"{b}{c}{_BLOCKS[i]}{colors['END']}"
+            
+
+        if sys.platform == "win32":
+            if r"\\" in dst or r"\\" in src:
                 chunk = 1024**2 * 64
-        elif sys.platform == 'linux':
+        elif sys.platform == "linux":
             if is_network_location_linux(dst):
                 chunk = 1024**2 * 64
 
@@ -216,16 +229,17 @@ if __name__ == '__main__':
         elif not os.path.isdir(src):
             # And dest is a dir
             if os.path.isdir(dst):
-                newFile = dst + '\\' + src.split('\\')[-1]
+                newFile = dst + "\\" + src.split("\\")[-1]
                 if not os.path.exists(newFile):
-                    copy_with_progress(src, f'{dst}\\{src.split('\\')[-1]}')
+                    copy_with_progress(src, dst + "\\" + src.split("\\")[-1])
+                    #copy_with_progress(src, f"{dst}\\{src.split('\\')[-1]}")
                 else:
-                    print(f'File {newFile} already exists')
+                    print(f"File {newFile} already exists")
             else:
                 # Dest is a file
                 copy_with_progress(src, dst)
         else:
-            print('Folder to folder, file to folder or file to file')
+            print("Folder to folder, file to folder or file to file")
     else:
-        print('Usage: python copyProg.py <source> <destination>')
-        print('  Source and destination can be files or directories')
+        print("Usage: python copyProg.py <source> <destination>")
+        print("  Source and destination can be files or directories")
